d7d26f24b3e871b8e40d1835c103374949c62792
Glen Pinner <glen.pinner@gmail.com>
2017-01-31 17:41:44 +0000

    Lesson 7.2: Route requests.

--- Lesson 7/play/express-server.js
+++ Lesson 7/play/express-server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/users/:userid', function (req, res) {
+    res.end(`You asked for user: ${req.params.userid}`);
+});
+
 app.get('*', function (req, res) {
     res.end('Hello, Express world!');
 });
